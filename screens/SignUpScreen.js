51786cbf8ac659b8b43062a643133d7ab2b05e87
EHilario <14129585+erichilario@users.noreply.github.com>
2020-09-30 01:58:55 +0930

    SplashScreen 100%, SignInScreen 50%

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
-const HelpScreen = ({ navigation }) => {
+const SignUpScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Help Screen</Text>
+      <Text>SignUp Screen</Text>
       <Button title="Click here" onPress={() => alert("Button clicked!")} />
     </View>
   );
 };
 
-export default HelpScreen;
+export default SignUpScreen;
 
 const styles = StyleSheet.create({
   container: {
